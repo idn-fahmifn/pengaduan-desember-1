1f39bf969f28710c754e3371439e2c64b65289f3
idn-fahmifn <fahmifn.idn@gmail.com>
2024-12-04 15:12:18 +0700

    create

--- resources/views/divisi/index.blade.php
+++ resources/views/divisi/index.blade.php
@@ -8,11 +8,15 @@
             <div class="card-body">
 
                 <div class="d-flex justify-content-between">
+                    <!-- judul -->
                     <h4 class="card-title">Divisi</h4>
-                    <a href="" class="btn btn-success">Tambah divisi</a>
+
+                    <!-- button -->
+                    <button type="button" class="btn btn-primary waves-effect waves-light" data-bs-toggle="modal"
+                        data-bs-target=".bs-example-modal-xl">Tambah Divisi</button>
                 </div>
 
-                
+
                 <p class="card-title-desc">Data Divisi</p>
 
                 <table id="datatable" class="table table-bordered dt-responsive nowrap"
@@ -32,6 +36,38 @@
         </div>
     </div>
     <!-- end col -->
+</div>
+
+<div class="modal fade bs-example-modal-xl" tabindex="-1" role="dialog"
+    aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title mt-0" id="myExtraLargeModalLabel">Tambah Divisi</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close">
+                </button>
+            </div>
+
+            <form action="" method="post">
+                @csrf
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Nama Divisi</label>
+                        <input type="text" name="nama_divisi" required class="form-control">
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Tambah</button>
+                </div>
+
+            </form>
+
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
 </div>
 
 @endsection
