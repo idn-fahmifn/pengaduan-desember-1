77bbd2e56d19fed189ebfc6d6e00a6aa4442e5de
idn-fahmifn <fahmifn.idn@gmail.com>
2024-12-04 13:25:43 +0700

    first

---
+++ database/migrations/2024_12_04_024842_create_biodata_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('biodata', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->string('no_hp');
+            $table->enum('gender', ['laki laki', 'perempuan']);
+            $table->text('alamat');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('biodata');
+    }
+};
---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // membuat akun admin
+
+        User::create([
+            // inout dibawah ga harus sama,
+            'name' => 'administrator',
+            'email' => 'admin@test.com',
+            'isAdmin' => 1,
+            'password' => 'asdasdasd'
+        ]);
+
+    }
+}
